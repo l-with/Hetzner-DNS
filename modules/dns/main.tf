e62ae5fc13f5ce4b4724f21032511caffb81a88c
Lars With <lars@with.de>
2022-01-22 07:06:28 +0100

    update provider to 2.0.0

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     hetznerdns = {
       source  = "timohirt/hetznerdns"
-      version = "1.1.1"
+      version = "2.0.0"
     }
   }
   required_version = ">= 0.14"
